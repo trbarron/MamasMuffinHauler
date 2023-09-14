b1e1d3d414f8b96c85d27359ef3c4561f0c9e93b
Tyler Barron <tyler.barron@oliveai.com>
2023-09-13 19:33:44 -0600

    whoops forgot to update photos

--- src/Photos.jsx
+++ src/Photos.jsx
@@ -6,18 +6,20 @@ import photo3 from './assets/photo3.jpg'
 import photo4 from './assets/photo4.jpg'
 import photo5 from './assets/photo5.jpg'
 import photo6 from './assets/photo6.jpg'
+import photo7 from './assets/photo7.jpg'
 
 function Photos() {
   return (
     <div className='min-h-screen w-screen bg-toast-500 grid grid-cols-1	'>
         <Navbar />
         <div>
-          <img src={photo1} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
-          <img src={photo2} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
-          <img src={photo3} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
-          <img src={photo4} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
-          <img src={photo5} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
-          <img src={photo6} className='aspect-auto m-auto lg:max-w-3xl py-4' alt='logo of a truck' />
+          <img src={photo1} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo2} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo3} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo4} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo5} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo6} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
+          <img src={photo7} className='aspect-auto m-auto lg:max-w-2xl py-4' alt='logo of a truck' />
 
         </div>
         <Footer />
